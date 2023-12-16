04050911040ccdbd2412d19295038f9f27539b1a
AbdulSalamBd705 <abdulsalambd705@gmail.com>
2023-12-16 13:43:41 +0600

    responsive

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const manu = document.querySelector('.manu');
 const icon = document.querySelector('.fa-solid');
 const body = document.querySelector('body');
 const slider = document.querySelector('.slider');
+const main_list = document.querySelector('.main_list');
 
 console.log(slider);
 
 manu.addEventListener('click', ()=>{
-  icon.classList.toggle('fa-xmark')
+  icon.classList.toggle('fa-xmark');
+  main_list.classList.toggle("show_list");
 })
 slider.addEventListener('click', ()=>{
   body.classList.toggle("active");
